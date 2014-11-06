1b3a31246f0b6541b4e998596b518b4589fbd8e6
thermatk <thermatk@thermatk.com>
2014-11-06 13:29:01 +0300

    checkstring added

--- app/src/main/java/com/thermatk/android/l/catchup/data/NesUpdateTimes.java
+++ app/src/main/java/com/thermatk/android/l/catchup/data/NesUpdateTimes.java
@@ -6,7 +6,7 @@ public class NesUpdateTimes extends SugarRecord<NesUpdateTimes> {
     public String type;
     long tableId;
     long lastUpdated;
-
+    String checkString;
 
     public NesUpdateTimes(){
     }
