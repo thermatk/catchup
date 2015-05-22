1acd1b185ca94415aef136e6788be1915c44a7ad
thermatk <thermatk@thermatk.com>
2015-05-22 12:26:04 +0300

    NavDrawer from library + orientation fix

--- app/src/main/java/com/thermatk/android/l/catchup/CatchUpMain.java
+++ app/src/main/java/com/thermatk/android/l/catchup/CatchUpMain.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
@@ -18,6 +18,10 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.Nameable;
 import com.thermatk.android.l.catchup.fragments.CourseFragment;
 import com.thermatk.android.l.catchup.fragments.DefaultFragment;
 import com.thermatk.android.l.catchup.fragments.LearnFragment;
@@ -25,14 +29,12 @@ import com.thermatk.android.l.catchup.fragments.ScheduleFragment;
 import com.thermatk.android.l.catchup.interfaces.CallbackListener;
 import com.thermatk.android.l.catchup.interfaces.UpdatableFragment;
 
-
-public class CatchUpMain extends ActionBarActivity implements CallbackListener {
-    private DrawerLayout mDrawerLayout;
-    private ListView mDrawerList;
-    private ActionBarDrawerToggle mDrawerToggle;
+import java.util.ArrayList;
 
 
-    private CharSequence mDrawerTitle;
+public class CatchUpMain extends AppCompatActivity implements CallbackListener {
+    private Drawer.Result result = null;
+
     private CharSequence mTitle;
     private String[] mNavMenuTitles;
 
@@ -44,6 +46,7 @@ public class CatchUpMain extends ActionBarActivity implements CallbackListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catch_up_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         if (toolbar != null) {
             setSupportActionBar(toolbar);
@@ -52,101 +55,82 @@ public class CatchUpMain extends ActionBarActivity implements CallbackListener {
         loadingBar = (ProgressBar) findViewById(R.id.progressBar);
         loadingBar.setVisibility(View.INVISIBLE);
 
-        // Check that the activity is using the layout version with
-        // the fragment_container FrameLayout
         if (findViewById(R.id.content_main_frame) != null) {
 
-            // However, if we're being restored from a previous state,
-            // then we don't need to do anything and should return or else
-            // we could end up with overlapping fragments.
             if (savedInstanceState != null) {
-                return;
+
+            } else {
+
+                DefaultFragment firstFragment = new DefaultFragment();
+
+                firstFragment.setArguments(getIntent().getExtras());
+
+                getFragmentManager().beginTransaction()
+                        .add(R.id.content_main_frame, firstFragment).commit();
             }
-
-            // Create a new Fragment to be placed in the activity layout
-            DefaultFragment firstFragment = new DefaultFragment();
-
-            // In case this activity was started with special instructions from an
-            // Intent, pass the Intent's extras to the fragment as arguments
-            firstFragment.setArguments(getIntent().getExtras());
-
-            // Add the fragment to the 'fragment_container' FrameLayout
-            getFragmentManager().beginTransaction()
-                    .add(R.id.content_main_frame, firstFragment).commit();
         }
 
 
         mNavMenuTitles = getResources().getStringArray(R.array.nav_menu_array);
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerList = (ListView) findViewById(R.id.left_drawer);
 
+        //////
+        ArrayList<IDrawerItem> drItems = new ArrayList<IDrawerItem>();
 
-        // set a custom shadow that overlays the main content when the drawer opens
-        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+        for(String item : mNavMenuTitles) {
+            drItems.add(new PrimaryDrawerItem().withName(item));
+        }
+        result = new Drawer()
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .withDisplayBelowToolbar(true)
+                .withTranslucentStatusBar(true)
+                .withActionBarDrawerToggle(true)
+                .withActionBarDrawerToggleAnimated(true)
+                .withDrawerItems(drItems)
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id, IDrawerItem drawerItem) {
+                        if (drawerItem != null && drawerItem instanceof Nameable) {
+                            //getSupportActionBar().setTitle(((Nameable) drawerItem).getNameRes());
+                            selectItem(position);
+                        }
+                    }
+                })
+                .withOnDrawerListener(new Drawer.OnDrawerListener() {
+                    @Override
+                    public void onDrawerOpened(View drawerView) {
 
-        // Set the adapter for the list view
-        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
-                R.layout.drawer_list_item, mNavMenuTitles));
-        // Set the list's click listener
-        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+                    }
 
-        mTitle = mDrawerTitle = getTitle();
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
-                toolbar, R.string.drawer_open, R.string.drawer_close) {
+                    @Override
+                    public void onDrawerClosed(View drawerView) {
 
-            /** Called when a drawer has settled in a completely closed state. */
-            public void onDrawerClosed(View view) {
-                super.onDrawerClosed(view);
-                getSupportActionBar().setTitle(mTitle);
-                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-            }
+                    }
 
-            /** Called when a drawer has settled in a completely open state. */
-            public void onDrawerOpened(View drawerView) {
-                super.onDrawerOpened(drawerView);
-                getSupportActionBar().setTitle(mDrawerTitle);
-                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-            }
-        };
+                    @Override
+                    public void onDrawerSlide(View drawerView, float slideOffset) {
 
-        // Set the drawer toggle as the DrawerListener
-        mDrawerLayout.setDrawerListener(mDrawerToggle);
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(true);
-    }
-
-    /* Called whenever we call invalidateOptionsMenu() */
-    @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
-        // If the nav drawer is open, hide action items related to the content view
-        boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
-        menu.findItem(R.id.menu_refresh).setVisible((!drawerOpen && loadingBar.getVisibility() != View.VISIBLE));
-        return super.onPrepareOptionsMenu(menu);
-    }
-
-
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        // Sync the toggle state after onRestoreInstanceState has occurred.
-        mDrawerToggle.syncState();
+                    }
+                })
+                .withFireOnInitialOnClick(true)
+                .withSavedInstance(savedInstanceState)
+                .build();
+        /////
     }
 
     @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        mDrawerToggle.onConfigurationChanged(newConfig);
+    protected void onSaveInstanceState(Bundle outState) {
+        outState = result.saveInstanceState(outState);
+        super.onSaveInstanceState(outState);
     }
 
-
-
-    /* The click listner for ListView in the navigation drawer */
-    private class DrawerItemClickListener implements ListView.OnItemClickListener {
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            selectItem(position);
+    @Override
+    public void onBackPressed() {
+        //handle the back press :D close the drawer first and if the drawer is closed close the activity
+        if (result != null && result.isDrawerOpen()) {
+            result.closeDrawer();
+        } else {
+            super.onBackPressed();
         }
     }
 
@@ -156,17 +140,14 @@ public class CatchUpMain extends ActionBarActivity implements CallbackListener {
             case 0: fragment = new DefaultFragment();break;
             case 1: fragment = new CourseFragment();break;
             case 2: fragment = new LearnFragment();break;
-            case 3: fragment = new ScheduleFragment();break;
+            case 3: fragment = new ScheduleFragment();
+                break;
             default: fragment = new DefaultFragment();
         }
 
         getFragmentManager().beginTransaction().replace(R.id.content_main_frame, fragment).commit();
 
-        // update selected item and title, then close the drawer
-
-        mDrawerList.setItemChecked(position, true);
         setTitle(mNavMenuTitles[position]);
-        mDrawerLayout.closeDrawer(mDrawerList);
     }
 
     @Override
@@ -178,7 +159,6 @@ public class CatchUpMain extends ActionBarActivity implements CallbackListener {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.catch_up_main, menu);
         this.menu = menu;
         return true;
@@ -186,17 +166,9 @@ public class CatchUpMain extends ActionBarActivity implements CallbackListener {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Pass the event to ActionBarDrawerToggle, if it returns
-        // true, then it has handled the app icon touch event
-        if (mDrawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             startActivity(new Intent(this, SettingsActivity.class));
             return true;
